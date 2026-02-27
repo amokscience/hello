4e5b11b76769ced10053376b594787f09d215a3f
Ken Ito <amokscience@hotmail.com>
2026-02-27 10:22:53 -0600

    Update greeting message in handleRoot function to combine "Happy Friday" and "Happy Weekend"

--- main.go
+++ main.go
@@ -134,7 +134,7 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	message := "Hello, World! Happy Friday. Happy Weekend"
+	message := "Hello, World! Happy Friday and Weekend"
 
 	// Get server name
 	serverName, _ := os.Hostname()
